708f723f0f2d76fc2947996bff7e0c4aa1360fd1
AlexanderSydorchuk <72271589+AlexanderSydorchuk@users.noreply.github.com>
2020-12-20 20:17:31 +0200

    Add files via upload

---
+++ OOP4/OOP4/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace OOP4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmb_Disks = new System.Windows.Forms.ComboBox();
+            this.lb_Disks = new System.Windows.Forms.Label();
+            this.b_Refresh = new System.Windows.Forms.Button();
+            this.lb_Type = new System.Windows.Forms.Label();
+            this.lb_Amount = new System.Windows.Forms.Label();
+            this.lb_Free = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lstbox_About = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // cmb_Disks
+            // 
+            this.cmb_Disks.FormattingEnabled = true;
+            this.cmb_Disks.Location = new System.Drawing.Point(68, 12);
+            this.cmb_Disks.Name = "cmb_Disks";
+            this.cmb_Disks.Size = new System.Drawing.Size(121, 21);
+            this.cmb_Disks.TabIndex = 0;
+            this.cmb_Disks.SelectedIndexChanged += new System.EventHandler(this.cmb_Disks_SelectedIndexChanged);
+            // 
+            // lb_Disks
+            // 
+            this.lb_Disks.AutoSize = true;
+            this.lb_Disks.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lb_Disks.Location = new System.Drawing.Point(12, 13);
+            this.lb_Disks.Name = "lb_Disks";
+            this.lb_Disks.Size = new System.Drawing.Size(50, 16);
+            this.lb_Disks.TabIndex = 1;
+            this.lb_Disks.Text = "Диски:";
+            // 
+            // b_Refresh
+            // 
+            this.b_Refresh.Location = new System.Drawing.Point(195, 10);
+            this.b_Refresh.Name = "b_Refresh";
+            this.b_Refresh.Size = new System.Drawing.Size(75, 23);
+            this.b_Refresh.TabIndex = 2;
+            this.b_Refresh.Text = "Оновити";
+            this.b_Refresh.UseVisualStyleBackColor = true;
+            this.b_Refresh.Click += new System.EventHandler(this.b_Refresh_Click);
+            // 
+            // lb_Type
+            // 
+            this.lb_Type.AutoSize = true;
+            this.lb_Type.Location = new System.Drawing.Point(12, 52);
+            this.lb_Type.Name = "lb_Type";
+            this.lb_Type.Size = new System.Drawing.Size(62, 13);
+            this.lb_Type.TabIndex = 3;
+            this.lb_Type.Text = "Тип диска:";
+            this.lb_Type.Visible = false;
+            // 
+            // lb_Amount
+            // 
+            this.lb_Amount.AutoSize = true;
+            this.lb_Amount.Location = new System.Drawing.Point(12, 75);
+            this.lb_Amount.Name = "lb_Amount";
+            this.lb_Amount.Size = new System.Drawing.Size(74, 13);
+            this.lb_Amount.TabIndex = 4;
+            this.lb_Amount.Text = "Обсяг диска:";
+            this.lb_Amount.Visible = false;
+            // 
+            // lb_Free
+            // 
+            this.lb_Free.AutoSize = true;
+            this.lb_Free.Location = new System.Drawing.Point(12, 99);
+            this.lb_Free.Name = "lb_Free";
+            this.lb_Free.Size = new System.Drawing.Size(74, 13);
+            this.lb_Free.TabIndex = 5;
+            this.lb_Free.Text = "Вільне місце:";
+            this.lb_Free.Visible = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(195, 55);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(274, 316);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // lstbox_About
+            // 
+            this.lstbox_About.FormattingEnabled = true;
+            this.lstbox_About.Location = new System.Drawing.Point(475, 55);
+            this.lstbox_About.Name = "lstbox_About";
+            this.lstbox_About.Size = new System.Drawing.Size(313, 134);
+            this.lstbox_About.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstbox_About);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lb_Free);
+            this.Controls.Add(this.lb_Amount);
+            this.Controls.Add(this.lb_Type);
+            this.Controls.Add(this.b_Refresh);
+            this.Controls.Add(this.lb_Disks);
+            this.Controls.Add(this.cmb_Disks);
+            this.Name = "Form1";
+            this.Text = "lstbox_Info";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmb_Disks;
+        private System.Windows.Forms.Label lb_Disks;
+        private System.Windows.Forms.Button b_Refresh;
+        private System.Windows.Forms.Label lb_Type;
+        private System.Windows.Forms.Label lb_Amount;
+        private System.Windows.Forms.Label lb_Free;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox lstbox_About;
+    }
+}
+
---
+++ OOP4/OOP4/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP4
+{
+    public partial class Form1 : Form
+    {
+        public DriveInfo[] drives = DriveInfo.GetDrives();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            b_Refresh_Click(sender, e);
+            int num = cmb_Disks.SelectedIndex;
+        }
+
+        private void b_Refresh_Click(object sender, EventArgs e)
+        {
+            cmb_Disks.Items.Clear();
+            foreach(DriveInfo drive in drives)
+            {
+                cmb_Disks.Items.Add(drives[0]);
+            }
+        }
+
+        private void cmb_Disks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int num = cmb_Disks.SelectedIndex;
+
+            if (drives[num].IsReady)
+            {
+                lb_Type.Visible = true;
+                lb_Free.Visible = true;
+                lb_Amount.Visible = true;
+
+
+                lb_Type.Text = $"Тип диска: {drives[num].DriveType}";
+                lb_Amount.Text = $"Обсяг диска: {drives[num].TotalSize}";
+                lb_Free.Text = $"Вільне місце: {drives[num].TotalFreeSpace}";
+            }
+            else
+            {
+                lb_Type.Visible = false;
+                lb_Free.Visible = false;
+                lb_Amount.Visible = false;
+            }
+
+
+            string directory_name = cmb_Disks.SelectedItem.ToString();
+
+            listBox1.Items.Clear();
+            if (Directory.Exists(directory_name))
+            {
+                string[] dir = Directory.GetDirectories(directory_name);
+                foreach (string item in dir)
+                {
+                    listBox1.Items.Add($"{item}");
+                }
+                string[] files = Directory.GetFiles(directory_name);
+
+                foreach (string item in files)
+                {
+                    listBox1.Items.Add($"{item}");
+                }
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string dir_name = listBox1.SelectedItem.ToString();
+            if (Directory.Exists(dir_name))
+            {
+                listBox1.Items.Clear();
+
+                string[] dirs = Directory.GetDirectories(dir_name);
+                foreach (string d in dirs)
+                {
+                    listBox1.Items.Add($"{d}");
+                }
+
+                string[] files = Directory.GetFiles(dir_name);
+                foreach (string file in files)
+                { 
+                    listBox1.Items.Add($"{file}");
+                }
+            
+
+            
+                DirectoryInfo info = new DirectoryInfo(dir_name);
+
+                lstbox_About.Items.Clear();
+                lstbox_About.Items.Add($"Диск: {info.Root}");
+                lstbox_About.Items.Add($"Назва папки: {info.Name}");
+                lstbox_About.Items.Add($"Дата створення: {info.CreationTime}");
+                lstbox_About.Items.Add($"Дата останнього відкриття: {info.LastAccessTime}");
+
+                long directory_size = DirectorySize(info);
+
+                lstbox_About.Items.Add($"Directory size: {directory_size} байт");
+            }
+        }
+
+        public static long DirectorySize(DirectoryInfo directory)
+        {
+            long size = 0;
+            
+            FileInfo[] files = directory.GetFiles();
+            foreach (FileInfo f in files)
+            {
+                size += f.Length;
+            }
+            
+            DirectoryInfo[] dirs = directory.GetDirectories();
+            foreach (DirectoryInfo d in dirs)
+            {
+                size += DirectorySize(d);
+            }
+            return (size);
+        }
+    }
+}
